bb0e4bfc673bab5d538f9e1f17c7cdc8a34a6625
eagantrp <eagant@nlcs.k12.in.us>
2018-08-17 11:14:29 -0400

    add linker, image, grabInput

--- fiddle_io.js
+++ fiddle_io.js
@@ -1,11 +1,33 @@
-function display(input,id) {
-	if (id) {
-	document.getElementById(id).innerHTML = input;
+function display(input,elementId) {
+	if (elementId) {
+	document.getElementById(elementId).innerHTML = input;
   }
   else {
   document.getElementById("test").innerHTML = input;
   }
 }
+
 function listener(elementId,eventId,event) {
 	document.getElementById(elementId).addEventListener(eventId,event)
+}
+
+function linker(url,elementId) {
+	document.getElementById(elementId).href = url
+}
+
+function image(url,elementId,width,height,alt) {
+	document.getElementById(elementId).src = url
+  if (width) {
+  	document.getElementById(elementId).width = width
+  }
+  if (height) {
+  	document.getElementById(elementId).height = height
+  }
+  if (alt) {
+  	document.getElementById(elementId).alt = alt
+  }
+}
+
+function grabInput(elementId) {
+	return document.getElementById(elementId).value
 }
